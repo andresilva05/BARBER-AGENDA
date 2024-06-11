e6ca81b109b414bc260235864f3d26fa7d7be3d6
andresilva05 <andresilva9657@gmail.com>
2024-06-11 11:32:49 -0300

    refactor: padronizar nomes de classes e funções, e comentários para explicações das funções.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-// Seleciona todos os botões na seção especificada
+// Seleciona todos os botões na seção especificada com a classe .button-section
 const buttons = document.querySelectorAll(".button-section");
-// Seleciona o modal
+
+// Seleciona o modal com o ID dialog-servicos
 const modal = document.getElementById("dialog-servicos");
-// Seleciona o botão de fechar dentro do modal
+
+// Seleciona o botão de fechar dentro do modal com o ID btn-close
 const closeX = document.getElementById("btn-close");
-// Seleciona o botão de voltar
+
+// Seleciona os botões de voltar com os IDs voltar-cortes e voltar-combos
 const voltarCortes = document.getElementById("voltar-cortes");
 const voltarCombos = document.getElementById("voltar-combos");
 
@@ -15,88 +18,95 @@ buttons.forEach((button) => {
   });
 });
 
-// Adiciona um evento para fechar o modal ao clicar no botão de fechar
+// Adiciona um evento para fechar o modal ao clicar no botão de fechar (X)
 closeX.addEventListener("click", function () {
   modal.close();
 });
 
-// Adiciona um evento para fechar o modal clicando fora dele
+// Adiciona um evento para fechar o modal ao clicar fora dele
 modal.addEventListener("click", function (event) {
   if (event.target === modal) {
     modal.close();
   }
 });
 
-// Seleciona todos os elementos com a classe .COMBO-CLICK
-const combo = document.getElementById("servico-combo");
+// Seleciona o elemento com o ID servico-combo
+const servicoCombo = document.getElementById("servico-combo");
+
+// Seleciona o elemento com o ID servico-cabelo
 const servicoCabelo = document.getElementById("servico-cabelo");
-const itensServicoCabelo = document.querySelectorAll(".itens-servico-cabelo")
-// Seleciona todos os elementos com a classe .ComBos
-const divCombos = document.querySelectorAll(".ComBos");
+
+// Seleciona todos os elementos com a classe .itens-servico-cabelo
+const itensServicoCabelo = document.querySelectorAll(".itens-servico-cabelo");
+
+// Seleciona todos os elementos com a classe .function-combo
+const itemServico = document.querySelectorAll(".function-combo");
+
 // Seleciona todos os elementos com a classe .click-combos
-const novoConteudo = document.querySelectorAll(".click-combos");
+const itensServicosCombo = document.querySelectorAll(".click-combos");
 
-const cabelo = document.getElementById("cabelo-click");
-
-
-// Adiciona um listener de clique ao elemento combo
-combo.addEventListener("click", () => {
+// Adiciona um listener de clique ao elemento servicoCombo para mostrar itens de combo
+servicoCombo.addEventListener("click", () => {
   console.log("Combo clicado");
 
-  // Esconde todos os elementos em divCombos
-  divCombos.forEach((element) => {
+  // Esconde todos os elementos com a classe .function-combo
+  itemServico.forEach((element) => {
     element.classList.add("hidden");
   });
 
-  // Mostra todos os elementos em novoConteudo
-  novoConteudo.forEach((element) => {
+  // Mostra todos os elementos com a classe .click-combos
+  itensServicosCombo.forEach((element) => {
     element.classList.remove("hidden");
   });
 
-  voltarCombos.classList.remove("hidden")
+  // Mostra o botão de voltar para combos
+  voltarCombos.classList.remove("hidden");
 });
 
-// Adiciona um evento para rolar para a div 'combo' ao clicar no botão de voltar
+// Adiciona um evento para mostrar os elementos de combo ao clicar no botão de voltar
 voltarCombos.addEventListener("click", () => {
-  divCombos.forEach((element) => {
+  // Mostra todos os elementos com a classe .function-combo
+  itemServico.forEach((element) => {
     element.classList.remove("hidden");
   });
 
-  // Mostra todos os elementos em novoConteudo
-  novoConteudo.forEach((element) => {
+  // Esconde todos os elementos com a classe .click-combos
+  itensServicosCombo.forEach((element) => {
     element.classList.add("hidden");
   });
-  voltarCombos.classList.add("hidden")
+
+  // Esconde o botão de voltar para combos
+  voltarCombos.classList.add("hidden");
 });
 
-
-// Adiciona um listener de clique ao elemento combo
+// Adiciona um listener de clique ao elemento servicoCabelo para mostrar itens de cabelo
 servicoCabelo.addEventListener("click", () => {
-  
-
-  // Esconde todos os elementos em divCombos
-  divCombos.forEach((element) => {
+  // Esconde todos os elementos com a classe .function-combo
+  itemServico.forEach((element) => {
     element.classList.add("hidden");
-  })
+  });
+
+  // Mostra todos os elementos com a classe .itens-servico-cabelo
   itensServicoCabelo.forEach((element) => {
     element.classList.remove("hidden");
-  })
-  
-  voltarCortes.classList.remove("hidden")
+  });
+
+  // Mostra o botão de voltar para cortes
+  voltarCortes.classList.remove("hidden");
 });
 
+// Adiciona um evento para mostrar os elementos de cabelo ao clicar no botão de voltar
 voltarCortes.addEventListener("click", () => {
-  divCombos.forEach((element) => {
+  // Mostra todos os elementos com a classe .function-combo
+  itemServico.forEach((element) => {
     element.classList.remove("hidden");
   });
 
-  // Mostra todos os elementos em novoConteudo
+  // Esconde todos os elementos com a classe .itens-servico-cabelo
   itensServicoCabelo.forEach((element) => {
     element.classList.add("hidden");
   });
-  voltarCortes.classList.add("hidden")
+
+  // Esconde o botão de voltar para cortes
+  voltarCortes.classList.add("hidden");
 });
-
-
-
-
